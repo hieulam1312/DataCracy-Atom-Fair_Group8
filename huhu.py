bafe5278e15844795118a03ae3ab3d152ad9fb0c
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-07-01 13:14:56 +0700

    Update huhu.py

--- huhu.py
+++ huhu.py
@@ -55,20 +55,31 @@ def check_student(df,id):
     st.set_option('deprecation.showPyplotGlobalUse', False)
     st.pyplot()
 
-def to_excel(df):
-    output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    df.to_excel(writer, sheet_name='Sheet1')
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
 
-def get_table_download_link(df):
-    val = to_excel(df)
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Download csv file</a>' # decode b'abc' => abc
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
 
-   
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+if st.button('Download input as a text file'):
+    tmp_download_link = download_link(s, 'YOUR_INPUT.txt', 'Click here to download your text!')
+    st.markdown(tmp_download_link, unsafe_allow_html=True)
 def transform(df,email_list):
   # SUMMARY
     index=st.sidebar.multiselect('Chosse index:',
@@ -198,7 +209,8 @@ def transform(df,email_list):
       file=list_BSEM
     file
     if st.button('Download here'):
-      get_table_download_link(file)
+      tmp_download_link = download_link(file, 'YOUR_DF.csv', 'Click here to download your data!')
+      st.markdown(tmp_download_link, unsafe_allow_html=True)
        
 def main():
     st.title('Student a dataset')
