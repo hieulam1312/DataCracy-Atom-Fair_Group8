1d70a1775258cc4641d793c765101d0dbada35f3
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-07-09 11:16:33 +0700

    Update operation4edu.py

--- operation4edu.py
+++ operation4edu.py
@@ -177,7 +177,7 @@ def clustering(df):
       print(Y)
       kmeans.fit(Y)
       Y["cluster"] = kmeans.labels_
-      sb.pairplot(data=Y,hue="cluster",palette='viridis',height=4)
+      sb.pairplot(data=Y,hue="cluster",palette='viridis',height=6)
       st.pyplot()
       
       Y["cluster"] = kmeans2.fit_predict(Y)
