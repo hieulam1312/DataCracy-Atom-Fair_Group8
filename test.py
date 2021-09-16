a4fe5137fef67986c4c0c0f3f2a5969429145412
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-09-16 20:25:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,51 +1,85 @@
-import asyncio
-
 import streamlit as st
-from httpx_oauth.clients.google import GoogleOAuth2
+import numpy as np
+from google.oauth2 import service_account
+import pandas as pd
+import pandas_gbq
+from googleapiclient.discovery import build
+from google.cloud import bigquery
+import json
+#import mysql.connector
+import os
+from PIL import Image
+import streamlit.components.v1 as components
+import streamlit.components.v1 as stc 
+import codecs
+import sys
 
-st.title("Google OAuth2 flow")
+# Create API client.
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets['gcp_service_account']
+# )
+# client = bigquery.Client(credentials=credentials)
 
-"## Configuration"
+#Streamlit app
+st.set_page_config(layout="wide")
 
-client_id = st.text_input("Client ID")
-client_secret = st.text_input("Client secret")
-redirect_uri = st.text_input("Redirect URI", "http://localhost:8000/redirect")
+st.title('HANDPICK CONCEPT COMPANY LIMITED')
+st.subheader('Data Warehouse - Google BigQuery')
+#image = Image.open('C:/Users/PC/Pictures/picture 3.png')
+#st.image(image, caption= 'Design by HandPickConcept', use_column_width=True)
 
-if client_id and client_secret and redirect_uri:
-    client = GoogleOAuth2(client_id, client_secret)
-else:
-    client = None
-    
-"## Authorization URL"
+st.sidebar.header('HOME')
+#image = Image.open('C:/Users/PC/Pictures/picture4.jpg')
+#st.sidebar.image(image,width=300)
+st.sidebar.subheader('About HANDPICK Concept')
+st.sidebar.write('**HANDPICK CONCEPT CO.,LTD**')
+st.sidebar.write('📌**Address**: S1 The Sun Avenue Tower, 28 Mai Chi Tho, An Phu Ward, Thu Duc, Ho Chi Minh City, Vietnam')
+st.sidebar.write('☎️ **Contact**: xxxxxxxxxx')
 
-async def write_authorization_url():
-    authorization_url = await client.get_authorization_url(
-        redirect_uri,
-        scope=["profile", "email"],
-        extras_params={"access_type": "offline"},
+st.subheader('HANDPICK DATA')
+
+s1 = u'ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚÝàáâãèéêìíòóôõùúýĂăĐđĨĩŨũƠơƯưẠạẢảẤấẦầẨẩẪẫẬậẮắẰằẲẳẴẵẶặẸẹẺẻẼẽẾếỀềỂểỄễỆệỈỉỊịỌọỎỏỐốỒồỔổỖỗỘộỚớỜờỞởỠỡỢợỤụỦủỨứỪừỬửỮữỰựỲỳỴỵỶỷỸỹ'
+s0 = u'AAAAEEEIIOOOOUUYaaaaeeeiioooouuyAaDdIiUuOoUuAaAaAaAaAaAaAaAaAaAaAaAaEeEeEeEeEeEeEeEeIiIiOoOoOoOoOoOoOoOoOoOoOoOoUuUuUuUuUuUuUuYyYyYyYy'
+def remove_accents(input_str):
+    s = ''
+    for c in input_str:
+        if c in s1:
+            s += s0[s1.index(c)]
+        else:
+            s += c
+    return s
+
+def normalize_db_table_column(df):
+    df.columns = df.columns.str.replace(' ', '_')
+    df.columns = df.columns.str.replace(r'\n', '_')
+    df.columns = df.columns.str.replace(r'[^\w\s]+', '_')
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.to_series().apply(remove_accents)
+
+
+#Setup push BigQuery
+def push_exit_table(df, db_table):
+    normalize_db_table_column(df)
+    print(df)
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"]
     )
-    st.write(authorization_url)
+    client = bigquery.Client(credentials=credentials)
 
-if client:
-    asyncio.run(write_authorization_url())
-else:
-    "Waiting client configuration..."
+    project_id="hp-data-324704"
 
-"## Callback"
+    pandas_gbq.to_gbq(df,f'DWhandpick.{db_table}', project_id=project_id, if_exists='append')
 
-if client:
-    code = st.text_input("Authorization code")
-else:
-    code = None
-    "Waiting client configuration..."
+    st.write("Please upload an Excel or CSV file")
 
-"## Access token"
 
-async def write_access_token(code):
-    token = await client.get_access_token(code, redirect_uri)
-    st.write(token)
+#checkbox
 
-if code:
-    asyncio.run(write_access_token(code))
-else:
-    "Waiting authorization code..."
+if st.checkbox("Import Files"):
+    db_table = st.selectbox("📍 Database Table 📍", ["Customer", "DO", "GO", "Inventory", "Product", "Production", "Return_SO", "SO", "Sup_Product"])
+    product_file = st.file_uploader(label="📤 Before selecting the file you want to import, please choosing 'Database Table' first 👆...", type = 'xlsx')
+    if product_file:
+        print('Process: ', product_file, db_table)
+        df1= pd.read_excel(product_file, engine = 'openpyxl')
+        st.dataframe(df1)
+        push_exit_table(df1, db_table)
