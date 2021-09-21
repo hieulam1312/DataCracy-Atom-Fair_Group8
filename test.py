fd0102deb0a58abb489a0c1e51082fd9e19c0ce5
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-09-21 10:37:34 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from numpy import histogram
 import streamlit as st
 import pandas as pd
-sample_ID=[st.text_input('Scan Mẫu')]
+sample_ID=[st.text_area('Scan Mẫu')]
 step=[st.selectbox('Chọn thao tác',['Trả mẫu','Mượn mẫu'])]
 
 history=pd.DataFrame(list(zip(sample_ID, step)),
